925824a182e998e49db37aca75872768661138f2
MrUnluckyy <Justas.Sobutas@gmail.com>
2022-05-22 10:23:19 +0300

    Add a backend to application

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
-console.log('Server...')
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 3000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'Welcome to the Support Desk API' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
