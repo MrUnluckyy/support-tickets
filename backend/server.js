56b8dfa3aef943c88ffa092eb2e8feb192cbf589
MrUnluckyy <Justas.Sobutas@gmail.com>
2022-07-31 10:46:24 +0300

    Fix production env

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-if (process.env.NODE_ENV === '') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
